467cdd1f3c957c875f104a26708e34bb6bace651
wayne <waynesouza07@gmail.com>
2022-03-30 21:14:55 -0300

    Adicionando biblioteca JPL para conexão entre Java e Prolog

---
+++ arvore.pl
@@ -0,0 +1,35 @@
+mae(ana, eva).
+mae(eva, noe).
+mae(bia, rai).
+mae(bia, clo).
+mae(bia, ary).
+mae(lia, gal).
+pai(ivo, eva).
+pai(rai, noe).
+pai(gil, rai).
+pai(gil, clo).
+pai(gil, ary).
+pai(ary, gal).
+mulher(ana).
+mulher(eva).
+mulher(bia).
+mulher(clo).
+mulher(lia).
+mulher(gal).
+homem(ivo).
+homem(rai).
+homem(noe).
+homem(gil).
+homem(ary).
+gerou(ana, eva).
+gerou(ivo, eva).
+gerou(eva, noe).
+gerou(rai, noe).
+gerou(bia, rai).
+gerou(bia, clo).
+gerou(bia, ary).
+gerou(gil, rai).
+gerou(gil, clo).
+gerou(gil, ary).
+gerou(ary, gal).
+gerou(lia, gal).
--- backend/src/main/java/com/trabalho/prolog/PrologApplication.java
+++ backend/src/main/java/com/trabalho/prolog/PrologApplication.java
@@ -1,13 +1,30 @@
 package com.trabalho.prolog;
 
+import org.jpl7.Atom;
+import org.jpl7.Query;
+import org.jpl7.Term;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class PrologApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrologApplication.class, args);
+		Query query = new Query("consult", new Term[] {
+				new Atom("~/Documentos/ifmg/trabalho-paradigmas/arvore.pl")
+		});
+		query.hasSolution();
+
+		Query q = new Query("gerou(ivo,X).");
+
+		Map<String, Term>[] res = q.allSolutions();
+
+		for(int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
 	}
 
 }
