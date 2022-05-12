bb31e4369c50cd49100ab6e6b4a2cb81091acdfb
waynesouza <waynesouza07@gmail.com>
2022-05-11 22:31:06 -0300

    Adicionando duas regras

--- arquivos/arvore.pl
+++ arquivos/arvore.pl
@@ -1,3 +1,7 @@
+% REGRAS
+compramos(X) :- gasto(_,X,_,_,_,_,_) , !.
+quantos(P,Q) :- findall(X,gasto(_,P,_,X,_,_,_),S) , sumlist(S,Q).
 
+% BASE DE CONHECIMENTO
 gasto('01-09-2021','cimento','loja a',20.0,'saco',28.0,560.0).
 gasto('02-02-2021','tijolo','loja b',2.0,'unidade',2.0,4.0).
